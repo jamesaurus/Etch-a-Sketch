2c6a8dccf56d9a2c950cd4b048d47e9d7a4b2fb3
James Ybanez <ybanezjames17@gmail.com>
2024-09-18 16:38:39 +1200

    Generate 16*16 grid

---
+++ script.js
@@ -0,0 +1,15 @@
+const container = document.querySelector("#container")
+const containerWidth = container.clientWidth;
+const containerHeight =  container.clientHeight;
+const column = 16;
+const row = 16;
+const gridBoxWidth = containerWidth/column;
+const gridBoxHeight = containerHeight/row;
+
+for(let i = 0; i < column * row; i++) {
+    const div = document.createElement("div");
+    div.classList.toggle("grid-box");
+    div.style.width = `${gridBoxWidth}px`;
+    div.style.height = `${gridBoxHeight}px`;
+    container.appendChild(div);
+}
